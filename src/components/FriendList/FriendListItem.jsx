202fcbdba2d678a2e1a12cae9c62c4c7dae03a8b
MilaY0109 <yanovskyl@ukr.net>
2024-03-12 10:23:29 +0200

    FriendslisItem render

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,17 +1,39 @@
+// import css from './FriendListItem.module.css';
+
+// const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <div>
+//       <img src={avatar} alt="Avatar" width="148" />
+//       <p>{name}</p>
+//       {isOnline ? (
+//         <p className={css.online}>Online</p>
+//       ) : (
+//         <p className={css.offline}>Offline</p>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FriendListItem;
+
 import css from './FriendListItem.module.css';
+
+
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
-      <img src={avatar} alt="Avatar" width="148" />
-      <p>{name}</p>
-      {isOnline ? (
+      <img className={css.friendAvatar} src={avatar} alt="Avatar" width="148" />
+      <p className={css.friendName}>{name}</p>
+      {/* {isOnline ? (
         <p className={css.online}>Online</p>
       ) : (
         <p className={css.offline}>Offline</p>
-      )}
+      )} */}
+        <p className={isOnline ? css.online : css.offline}>
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
     </div>
   );
 };
-
 export default FriendListItem;
